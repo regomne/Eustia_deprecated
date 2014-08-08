2c69a1e2f55ec79a10f6527fea1d1edf607e69de
regomne <fallingsunz@gmail.com>
2014-08-08 21:55:15 +0800

    ..

--- worker/Memory.cpp
+++ worker/Memory.cpp
@@ -1,9 +1,11 @@
 #include "Memory.h"
 #include "../gs/ToolFun.h"
+#include <string>
 
+using namespace std;
 BOOL GetMemoryBlocks(std::vector<MEMORY_BASIC_INFORMATION>& blocks)
 {
-    for (DWORD addr = 0; addr<=0x80000000;)
+    for (DWORD addr = 0; addr<0x80000000;)
     {
         /*if (IsBadReadPtr((void*)addr, 1))
         {
@@ -58,4 +60,28 @@ BOOL GetAPIAddress(wchar_t* moduleName, char* funcName, PVOID* addr)
         }
     }
     return FALSE;
+}
+
+BOOL DumpMemory(HANDLE process, void* start, int size, wchar_t* fileName)
+{
+    auto hf = CreateFile(fileName, GENERIC_WRITE, FILE_SHARE_READ, 0, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
+    if (hf == INVALID_HANDLE_VALUE)
+    {
+        DBGOUT(("Can't open dump file. err: %d", GetLastError()));
+        return FALSE;
+    }
+
+    auto buff = new BYTE[0x10000];
+    int leftBytes = size;
+    BYTE* curStart = (BYTE*)start;
+    while (leftBytes > 0)
+    {
+        SIZE_T readBytes=0;
+        auto ret=ReadProcessMemory(process, curStart, buff, (leftBytes > 0x10000 ? 0x10000 : leftBytes), &readBytes);
+        if (ret==0)
+        {
+            DBGOUT(("Can't read memory. addr:%x", curStart));
+            return FALSE;
+        }
+    }
 }
